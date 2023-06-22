8b89e94dba0a7a6f6c0796f2e49f5f1d8519ed71
trmttm <96355189+trmttm@users.noreply.github.com>
2023-06-23 08:31:46 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='nMemento',
+    version='',
+    packages=['test', 'n_memento'],
+    url='',
+    license='',
+    author='taro',
+    author_email='t.yamaka.1213@gmail.com',
+    description=''
+)
